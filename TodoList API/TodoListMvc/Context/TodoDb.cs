1f1ff5c0f383c0f02cdb7963df75a9721d5585cd
João Victor Lopes Lopes <20221BSI0439@cefetes.br>
2024-10-23 08:55:46 -0300

    remove os métodos do contexto para melhor abstração

--- TodoList API/TodoListMvc/Context/TodoDb.cs
+++ TodoList API/TodoListMvc/Context/TodoDb.cs
@@ -10,44 +10,5 @@ namespace TodoListMvc.Context
             : base(options) { }
 
         public DbSet<Todo> Todos => Set<Todo>();
-
-        public Todo Create(Todo todo)
-        {
-            Todos.Add(todo);
-            SaveChanges();
-            return todo;
-        }
-
-        public Todo GetById(int id)
-        {
-            return Todos.Where(todo => todo.Id == id).FirstOrDefault();
-        }
-
-        public void Delete(int id)
-        {
-            if (GetById(id) == null)
-                throw new Exception("Não foi possível deletar, não existe um Todo Item para esse Id.");
-
-            Todos.Remove(GetById(id));
-            SaveChanges();
-        }
-
-        public Todo Update(int id, Todo todo)
-        {
-            Todo objParaAtualizar = GetById(id);
-
-            objParaAtualizar.Name = todo.Name;
-            objParaAtualizar.IsComplete = todo.IsComplete;
-
-            SaveChanges();
-
-            return objParaAtualizar;
-        }
-
-        public IQueryable<Todo> GetAll()
-        {
-            return Todos;
-        }
-
     }
 }
