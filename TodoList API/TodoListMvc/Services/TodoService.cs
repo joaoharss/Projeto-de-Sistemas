6f16d7019067dd476790df5f0d4dbe349b28ef23
João Victor Lopes Lopes <20221BSI0439@cefetes.br>
2024-10-23 08:53:57 -0300

    remove injecao de dependencia do contexto e agora passa a apontar para o repositório

--- TodoList API/TodoListMvc/Services/TodoService.cs
+++ TodoList API/TodoListMvc/Services/TodoService.cs
@@ -1,47 +1,49 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
 using TodoListMvc.Context;
 using TodoListMvc.Contracts;
 using TodoListMvc.Model;
+using TodoListMvc.Repository;
 
 namespace TodoListMvc.Services
 {
     public class TodoService : ITodo
     {
-        private readonly Context.TodoDb _dbContext;
+            private readonly TodoRepository _todoRepository;
 
-        public TodoService(TodoDb dbContext)
-        {
-            _dbContext = dbContext;
-        }
+            public TodoService(TodoRepository todoRepository)
+            {
+                _todoRepository = todoRepository;
+            }
 
-        public Todo Create(Todo todo)
-        {
-            return _dbContext.Create(todo);
-        }
+            public Todo Create(Todo todo)
+            {
+                return _todoRepository.Create(todo);
+            }
 
-        public void Delete(int id)
-        {
-            _dbContext.Delete(id);
-        }
+            public void Delete(int id)
+            {
+                _todoRepository.Delete(id);
+            }
 
-        public IEnumerable<Todo> Search()
-        {
-            return _dbContext.GetAll().ToList();
-        }
+            public IEnumerable<Todo> Search()
+            {
+                return _todoRepository.GetAll().ToList();
+            }
 
-        public Todo SearchById(int id)
-        {
-            return _dbContext.GetById(id);
-        }
+            public Todo SearchById(int id)
+            {
+                return _todoRepository.GetById(id);
+            }
 
-        public IEnumerable<Todo> SearchComplete()
-        {
-            return _dbContext.GetAll().Where(todo => todo.IsComplete);
-        }
+            public IEnumerable<Todo> SearchComplete()
+            {
+                return _todoRepository.GetAll().Where(todo => todo.IsComplete);
+            }
 
-        public Todo Update(int id, Todo todo)
-        {
-            return _dbContext.Update(id, todo);
+            public Todo Update(int id, Todo todo)
+            {
+                return _todoRepository.Update(id, todo);
+            }
         }
-    }
 }
