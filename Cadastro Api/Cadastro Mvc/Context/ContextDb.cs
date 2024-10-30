d5c5156b29078d68f909502c6b5d45f4220caeda
Administrator <lemelting53@gmail.com>
2024-10-30 00:32:33 -0300

    Criação do contexto e mapeamento das classes.

---
+++ Cadastro Api/Cadastro Mvc/Context/ContextDb.cs
@@ -0,0 +1,27 @@
+﻿using Cadastro_Mvc.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cadastro_Mvc.Context
+{
+    public class ContextDb : DbContext
+    {
+        public ContextDb(DbContextOptions<ContextDb> options)
+            : base(options) { }
+
+        public DbSet<Pessoa> Pessoas => Set<Pessoa>();
+        public DbSet<Endereco> Enderecos => Set<Endereco>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Pessoa>()
+                .HasMany(p => p.Enderecos)
+                .WithOne(e => e.Pessoa)
+                .HasForeignKey(e => e.IdPessoa)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Endereco>()
+                .Property(e => e.IdPessoa)
+                .IsRequired(false);
+        }
+    }
+}
