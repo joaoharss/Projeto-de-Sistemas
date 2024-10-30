c6c274002ae8a9655f4be941c15d8412f2bdce09
Administrator <lemelting53@gmail.com>
2024-10-30 00:33:43 -0300

    incluindo as dependências no program.cs para que possa ser possível a injeção de dependências nos assemblies.

--- Cadastro Api/Cadastro Mvc/Program.cs
+++ Cadastro Api/Cadastro Mvc/Program.cs
@@ -1,3 +1,8 @@
+﻿using Cadastro_Mvc.Context;
+using Cadastro_Mvc.Contracts;
+using Cadastro_Mvc.Repository;
+using Cadastro_Mvc.Services;
+using Microsoft.EntityFrameworkCore;
 
 namespace Cadastro_Mvc
 {
@@ -7,16 +12,20 @@ namespace Cadastro_Mvc
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddDbContext<ContextDb>(opt => opt.UseInMemoryDatabase("CadastroList"));
+            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+            builder.Services.AddScoped<IPessoa, PessoaService>();
+            builder.Services.AddScoped<IEndereco, EnderecoService>();
+
+            builder.Services.AddScoped<EnderecoRepository, EnderecoRepository>();
+            builder.Services.AddScoped<PessoaRepository, PessoaRepository>();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
