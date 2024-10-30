827d8b1c3c84e681d54bab1eea4db8d9bbfa57d5
Administrator <lemelting53@gmail.com>
2024-10-30 00:35:02 -0300

    repositórios.

---
+++ Cadastro Api/Cadastro Mvc/Repository/EnderecoRepository.cs
@@ -0,0 +1,30 @@
+﻿using Cadastro_Mvc.Context;
+using Cadastro_Mvc.Contracts;
+using Cadastro_Mvc.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cadastro_Mvc.Repository
+{
+    public class EnderecoRepository
+    {
+        private readonly ContextDb _dbContext;
+
+        public EnderecoRepository (ContextDb dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Endereco Create(Endereco endereco)
+        {
+            _dbContext.Enderecos.Add(endereco);
+            _dbContext.SaveChanges(); 
+
+            // Após salvar, busca o endereço criado com a pessoa associada
+            var enderecoComPessoa = _dbContext.Enderecos
+                .Include(e => e.Pessoa) 
+                .FirstOrDefault(e => e.Id == endereco.Id);
+
+            return enderecoComPessoa; 
+        }
+    }
+}
---
+++ Cadastro Api/Cadastro Mvc/Repository/PessoaRepository.cs
@@ -0,0 +1,34 @@
+﻿using Cadastro_Mvc.Context;
+using Cadastro_Mvc.Contracts;
+using Cadastro_Mvc.Model;
+using Microsoft.EntityFrameworkCore; // Certifique-se de que você tem isso para usar Include
+
+
+namespace Cadastro_Mvc.Repository
+{
+    public class PessoaRepository
+    {
+        private readonly ContextDb _dbContext;
+
+        public PessoaRepository(ContextDb dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Pessoa Create(Pessoa pessoa)
+        {
+            _dbContext.Pessoas.Add(pessoa);
+            _dbContext.SaveChanges();
+
+            return pessoa;
+        }
+
+        public Pessoa GetById(int id)
+        {
+            var pessoa = _dbContext.Pessoas
+                .Include(w => w.Enderecos)
+                .Where(todo => todo.Id == id).FirstOrDefault();
+            return pessoa;
+        }
+    }
+}
