0c0a8f1d4dab61f42d1bcb5620aced0b5ab1386a
Administrator <lemelting53@gmail.com>
2024-10-29 21:01:07 -0300

    cria novo diretório para atividade nova, adiciona pastas ao projeto e cria as classes

---
+++ Cadastro Api/Cadastro Mvc/Model/Endereco.cs
@@ -0,0 +1,13 @@
+﻿namespace Cadastro_Mvc.Model
+{
+    public class Endereco
+    {
+        //Logradouro, número, estado (UF), cidade e Bairro)
+        public int Id { get; set; }
+        public string Logradouro { get; set; }
+        public int Numero { get; set; }
+        public string UF { get; set; }
+        public string Cidade { get; set; }
+        public string Bairro { get; set; }
+    }
+}
---
+++ Cadastro Api/Cadastro Mvc/Model/Pessoa.cs
@@ -0,0 +1,11 @@
+﻿namespace Cadastro_Mvc.Model
+{
+    public class Pessoa
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public string Email { get; set; }
+        public ICollection<Endereco> Enderecos { get; set; }
+    }
+}
