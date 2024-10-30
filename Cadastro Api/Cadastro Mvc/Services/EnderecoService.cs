98069a155776d268d00679ef6eab8b49bb2bdddd
Administrator <lemelting53@gmail.com>
2024-10-30 00:34:29 -0300

    serviço das classes criadas.

---
+++ Cadastro Api/Cadastro Mvc/Services/EnderecoService.cs
@@ -0,0 +1,26 @@
+﻿using Cadastro_Mvc.Contracts;
+using Cadastro_Mvc.Model;
+using Cadastro_Mvc.Repository;
+using Cadastro_Mvc.Util;
+
+namespace Cadastro_Mvc.Services
+{
+    public class EnderecoService : IEndereco
+    {
+        private readonly EnderecoRepository _enderecoRepository;
+        private readonly PessoaRepository _pessoaRepository;
+        public EnderecoService(EnderecoRepository enderecoRepository, PessoaRepository pessoaRepository)
+        {
+            _enderecoRepository = enderecoRepository;
+            _pessoaRepository = pessoaRepository;
+
+        }
+        public Endereco Create(Endereco endereco)
+        {
+            if (_pessoaRepository.GetById(endereco.IdPessoa).IsNull())
+                throw new NullReferenceException("Essa pessoa não existe.");
+
+            return _enderecoRepository.Create(endereco);
+        }
+    }
+}
---
+++ Cadastro Api/Cadastro Mvc/Services/PessoaService.cs
@@ -0,0 +1,25 @@
+﻿using Cadastro_Mvc.Contracts;
+using Cadastro_Mvc.Model;
+using Cadastro_Mvc.Repository;
+
+namespace Cadastro_Mvc.Services
+{
+    public class PessoaService : IPessoa
+    {
+        private readonly PessoaRepository _pessoaRepository;
+
+        public PessoaService(PessoaRepository pessoaRepository)
+        {
+            _pessoaRepository = pessoaRepository;
+        }
+        public Pessoa Create(Pessoa pessoa)
+        {
+            return _pessoaRepository.Create(pessoa);
+        }
+
+        public Pessoa GetById(int id)
+        {
+            return _pessoaRepository.GetById(id);
+        }
+    }
+}
