2487bd095ccdcc11a2e50d5a8a1eb1abf5323fd9
Administrator <lemelting53@gmail.com>
2024-10-30 00:33:02 -0300

    adição das controllers

---
+++ Cadastro Api/Cadastro Mvc/Controllers/EnderecoController.cs
@@ -0,0 +1,28 @@
+﻿using Cadastro_Mvc.Contracts;
+using Cadastro_Mvc.Model;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using static Cadastro_Mvc.Util.Controller;
+using static Cadastro_Mvc.Util.Methods;
+
+namespace Cadastro_Mvc.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EnderecoController : Controller
+    {
+        private readonly IEndereco _enderecoContract;
+
+        public EnderecoController(IEndereco enderecoContract)
+        {
+            _enderecoContract = enderecoContract;
+        }
+
+        [HttpPost("/Endereco/Create/")]
+        public ActionResult<Endereco> Create([FromBody] Endereco endereco)
+        {
+            return JsonOptionsUtil.Create(_enderecoContract.Create(endereco));
+        }
+    }
+}
---
+++ Cadastro Api/Cadastro Mvc/Controllers/PessoaController.cs
@@ -0,0 +1,33 @@
+﻿using Cadastro_Mvc.Contracts;
+using Cadastro_Mvc.Model;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using static Cadastro_Mvc.Util.Controller;
+
+namespace Cadastro_Mvc.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PessoaController : Controller
+    {
+        private readonly IPessoa _pessoaContract;
+
+        public PessoaController(IPessoa pessoaContract)
+        {
+            _pessoaContract = pessoaContract;
+        }
+
+        [HttpPost("/Create/")]
+        public ActionResult<Pessoa> Create([FromBody] Pessoa pessoa)
+        {
+            return JsonOptionsUtil.Create(_pessoaContract.Create(pessoa));
+        }
+
+        [HttpGet("/GetById/{id}")]
+        public ActionResult<Pessoa> GetById(int id)
+        {
+            return JsonOptionsUtil.Create(_pessoaContract.GetById(id));
+        }
+    }
+}
