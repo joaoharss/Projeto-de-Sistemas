7fe1d1d286cf342d98cb737e6d63193c40d1e3d3
Administrator <lemelting53@gmail.com>
2024-10-30 00:32:53 -0300

    criação dos contratos com seus respectivos métodos.

---
+++ Cadastro Api/Cadastro Mvc/Contracts/IEndereco.cs
@@ -0,0 +1,9 @@
+﻿using Cadastro_Mvc.Model;
+
+namespace Cadastro_Mvc.Contracts
+{
+    public interface IEndereco
+    {
+        Endereco Create(Endereco endereco);
+    }
+}
---
+++ Cadastro Api/Cadastro Mvc/Contracts/IPessoa.cs
@@ -0,0 +1,27 @@
+﻿using Cadastro_Mvc.Model;
+using System.Runtime.Serialization;
+
+namespace Cadastro_Mvc.Contracts
+{
+    public interface IPessoa
+    {
+        Pessoa Create(Pessoa pessoa);
+
+        Pessoa GetById(int id);
+    }
+
+    [DataContract]
+    public class PessoaDc
+    {
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember]
+        public string Nome { get; set; }
+        [DataMember]
+        public string Email { get; set;}
+        [DataMember]
+        public int Idade { get; set; }
+        [DataMember]
+        public IEnumerable<Endereco> Enderecos { get; set; }
+    }
+}
