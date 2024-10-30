fb31a597e84a7b51726361ecaa294238fd130054
Administrator <lemelting53@gmail.com>
2024-10-30 00:34:15 -0300

    criação de um diretório util para evitar repetição de métodos.

---
+++ Cadastro Api/Cadastro Mvc/Util/Controller.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json.Serialization;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cadastro_Mvc.Util
+{
+    /// <summary>
+    /// Classe criada para auxiliar o retorno do JSON devido à referência cíclica da classe Endereço na classe Pessoa.
+    /// </summary>
+    public static class Controller
+    {
+        public static class JsonOptionsUtil
+        {
+            private static readonly JsonSerializerOptions DefaultOptions = new JsonSerializerOptions
+            {
+                ReferenceHandler = ReferenceHandler.IgnoreCycles,
+                WriteIndented = true 
+            };
+
+            public static JsonResult Create<T>(T data)
+            {
+                return new JsonResult(data, DefaultOptions);
+            }
+        }
+    }
+}
---
+++ Cadastro Api/Cadastro Mvc/Util/Methods.cs
@@ -0,0 +1,10 @@
+﻿namespace Cadastro_Mvc.Util
+{
+    public static class Methods
+    {
+        public static bool IsNull(this object source)
+        {
+            return source == null;
+        }
+    }
+}
